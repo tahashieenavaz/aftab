63fbb7e2e1e662a3fbaea19c4a5f90f42177c35d
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-11 12:18:42 +0200

    adds EMA to init

--- aftab/modules/__init__.py
+++ aftab/modules/__init__.py
@@ -1,3 +1,4 @@
 from .LayerNorm2d import LayerNorm2d
 from .Stream import Stream
 from .SimpleAttention import SimpleAttention
+from .EfficientMultiScaleAttention import EfficientMultiScaleAttention
