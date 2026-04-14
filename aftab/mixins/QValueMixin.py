a5c8ab341f87c884081448b818271a791e02dce6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-14 14:46:11 +0200

    updates no_grad arg to gradient

--- aftab/mixins/QValueMixin.py
+++ aftab/mixins/QValueMixin.py
@@ -5,11 +5,10 @@ class QValueMixin:
     def __init__(self):
         super().__init__()
 
-    def get_q_values(self, float_observations, no_grad: bool = False):
-        if no_grad:
+    def get_q_values(self, float_observations, gradient: bool = False):
+        if not gradient:
             with torch.no_grad():
                 q_values = self._network(float_observations)
         else:
             q_values = self._network(float_observations)
-
         return q_values
