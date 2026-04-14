a07997f83fb74d1800df57160d1676d5f7f7137a
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-14 14:09:28 +0200

    refines training results mixin

--- aftab/mixins/TrainingResultsMixin.py
+++ aftab/mixins/TrainingResultsMixin.py
@@ -5,26 +5,27 @@ class TrainingResultsMixin:
     def __init__(self):
         super().__init__()
 
-    # TODO: change this to use simple namespace of self.results
     def flush_final_properties(self):
         self.final_training_rewards = None
         self.final_test_rewards = None
         self.final_loss_evolution = None
         self.final_duration = None
 
-    def make_log_filename(self, **arguments):
-        filename = "_".join(f"{k}-{v}" for k, v in arguments.items())
-        extension = "pkl"
-        return f"{filename}.{extension}"
+    def make_log_filename(self, **kwargs):
+        filename = "_".join(f"{k}-{v}" for k, v in kwargs.items())
+        return f"{filename}.pkl"
 
-    def save(self, **arguments) -> None:
-        funnel(
-            self.make_log_filename(**arguments),
-            {
-                "training_reward": self.final_training_rewards,
-                "test_reward": self.final_test_rewards,
-                "loss": self.final_loss_evolution,
-                "duration_seconds": self.final_duration,
-                "duration_hours": self.final_duration / 3600,
-            },
-        )
+    def _build_log_payload(self):
+        duration = self.final_duration or 0
+        return {
+            "training_reward": self.final_training_rewards,
+            "test_reward": self.final_test_rewards,
+            "loss": self.final_loss_evolution,
+            "duration_seconds": duration,
+            "duration_hours": duration / 3600,
+        }
+
+    def save(self, **kwargs) -> None:
+        filename = self.make_log_filename(**kwargs)
+        payload = self._build_log_payload()
+        funnel(filename, payload)
