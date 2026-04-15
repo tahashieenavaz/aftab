b5e72706c780660109615cbd48fa75d50b2ee522
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-15 18:52:57 +0200

    rewrites epsilon

--- aftab/encoders/EpsilonEncoder.py
+++ aftab/encoders/EpsilonEncoder.py
@@ -1,5 +1,5 @@
 import torch
-from ..modules import LayerNorm2d
+from ..modules import EncoderBlock
 from ..constants import ModuleType
 
 
@@ -7,18 +7,18 @@ class EpsilonEncoder(torch.nn.Module):
     def __init__(self, *, activation: ModuleType = torch.nn.ReLU):
         super().__init__()
         self.stream = torch.nn.Sequential(
-            torch.nn.Conv2d(4, 32, 3, 2, 1),
-            LayerNorm2d(32),
-            activation(),
-            torch.nn.Conv2d(32, 48, 3, 2, 1),
-            LayerNorm2d(48),
-            activation(),
-            torch.nn.Conv2d(48, 64, 3, 2, 0),
-            LayerNorm2d(64),
-            activation(),
-            torch.nn.Conv2d(64, 64, 3, 1, 0),
-            LayerNorm2d(64),
-            activation(),
+            EncoderBlock(
+                4, 32, kernel_size=3, stride=2, padding=1, activation=activation
+            ),
+            EncoderBlock(
+                32, 48, kernel_size=3, stride=2, padding=1, activation=activation
+            ),
+            EncoderBlock(
+                48, 64, kernel_size=3, stride=2, padding=0, activation=activation
+            ),
+            EncoderBlock(
+                64, 64, kernel_size=3, stride=1, padding=0, activation=activation
+            ),
             torch.nn.Flatten(),
         )
 
