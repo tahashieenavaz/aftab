091b88adec7573d9f24e4eea4eeb0b06847b7ba7
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-15 12:41:46 +0200

    removes extra modules

--- aftab/modules/__init__.py
+++ aftab/modules/__init__.py
@@ -1,5 +1,2 @@
 from .LayerNorm2d import LayerNorm2d
 from .Stream import Stream
-from .SimpleAttention import SimpleAttention
-from .EfficientMultiScaleAttention import EfficientMultiScaleAttention
-from .CoordinateAttention import CoordinateAttention
