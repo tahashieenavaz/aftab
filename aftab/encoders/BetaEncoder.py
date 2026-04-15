9b023b2171926755a75d70cd919650335fe58fb8
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-15 18:50:47 +0200

    rewrites beta

--- aftab/encoders/BetaEncoder.py
+++ aftab/encoders/BetaEncoder.py
@@ -1,5 +1,5 @@
 import torch
-from ..modules import LayerNorm2d
+from ..modules import EncoderBlock
 from ..constants import ModuleType
 
 
@@ -7,18 +7,18 @@ class BetaEncoder(torch.nn.Module):
     def __init__(self, *, activation: ModuleType = torch.nn.ReLU):
         super().__init__()
         self.stream = torch.nn.Sequential(
-            torch.nn.Conv2d(4, 32, kernel_size=6, stride=2, padding=2),
-            LayerNorm2d(32),
-            activation(),
-            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            LayerNorm2d(64),
-            activation(),
-            torch.nn.Conv2d(64, 32, kernel_size=4, stride=2, padding=1),
-            LayerNorm2d(32),
-            activation(),
-            torch.nn.Conv2d(32, 16, kernel_size=8, stride=1, padding=0),
-            LayerNorm2d(16),
-            activation(),
+            EncoderBlock(
+                4, 32, kernel_size=6, stride=2, padding=2, activation=activation
+            ),
+            EncoderBlock(
+                32, 64, kernel_size=3, stride=1, padding=1, activation=activation
+            ),
+            EncoderBlock(
+                64, 32, kernel_size=4, stride=2, padding=1, activation=activation
+            ),
+            EncoderBlock(
+                32, 16, kernel_size=8, stride=1, padding=0, activation=activation
+            ),
             torch.nn.Flatten(),
         )
 
