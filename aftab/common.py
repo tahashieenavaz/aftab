ae99304dc0deccbddee5193c2d833b87b4652e8b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-16 15:02:09 +0200

    renames the variable

--- aftab/common.py
+++ aftab/common.py
@@ -1,15 +1,13 @@
 class LinearEpsilon:
-    def __init__(
-        self, ratio: float = 0.1, minimum: float = 0.001, maximum: float = 1.0
-    ):
+    def __init__(self, span: float = 0.1, minimum: float = 0.001, maximum: float = 1.0):
         self.maximum = maximum
         self.minimum = minimum
-        self.ratio = ratio
+        self.span = span
 
     def get(self, frames, total_frames):
         minimum = self.minimum
         maximum = self.maximum
-        decay_duration = total_frames * self.ratio
+        decay_duration = total_frames * self.span
 
         if decay_duration == 0:
             return maximum
