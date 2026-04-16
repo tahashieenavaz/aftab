cc774c195f9c33b73ebca5b22ba4d3f58f60efe2
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-16 15:45:54 +0200

    improves creation criteria

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,4 +1,4 @@
 source ~/.bashrc
-micromamba create -n aftab_experiments python=3.11
+micromamba create -n aftab_experiments -c conda-forge python=3.11 pip
 micromamba activate aftab_experiments
 pip install aftab
