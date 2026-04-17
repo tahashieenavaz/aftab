b0bda32fde35ae324c5c4bd717cbe66dd243b4b1
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 12:52:08 +0200

    wip

--- aftab/modules/augmentation/ColorIntensity.py
+++ aftab/modules/augmentation/ColorIntensity.py
@@ -6,7 +6,7 @@ class ColorIntensity(torch.nn.Module):
         super().__init__()
         self.scale = scale
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         if not self.training:
             return x
         noise = 1.0 + self.scale * torch.randn((x.shape[0], 1, 1, 1), device=x.device)
