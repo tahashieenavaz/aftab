1cb1fbe3ccfc60d1268652ab6070ee79b212aadb
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-17 13:45:18 +0200

    optimizer mixin refined

--- aftab/mixins/OptimizerMixin.py
+++ aftab/mixins/OptimizerMixin.py
@@ -1,25 +1,29 @@
-import torch
-
-
 class OptimizerMixin:
     def __init__(self):
         super().__init__()
 
     def __build_quantile_optimizers(self):
-        quantile_value_optimizer = torch.optim.RAdam(
-            list(self._network.phi.parameters())
-            + list(self._network.quantile_value.parameters()),
+        image_encoder_parameters = list(self._network.phi.parameters())
+        quantile_value_parameters = list(self._network.quantile_value.parameters())
+        fraction_proposal_parameters = list(
+            self._network.fraction_proposal.parameters()
+        )
+        ordinary_parameters = image_encoder_parameters + quantile_value_parameters
+
+        quantile_value_optimizer = self.optimizer_instance(
+            ordinary_parameters,
             lr=self.lr,
             eps=self.optimizer_epsilon,
         )
-        fraction_proposal_optimizer = torch.optim.RAdam(
-            self._network.fraction_proposal.parameters(),
+        fraction_proposal_optimizer = self.optimizer_instance(
+            fraction_proposal_parameters,
             lr=self.fraction_proposal_lr,
             eps=self.optimizer_epsilon,
         )
+
         return fraction_proposal_optimizer, quantile_value_optimizer
 
-    def make_optimizer(self):
+    def __build_regression_optimizers(self):
         return self.optimizer_instance(
             self._network.parameters(),
             lr=self.lr,
@@ -27,3 +31,9 @@ class OptimizerMixin:
             betas=(self.optimizer_first_beta, self.optimizer_second_beta),
             weight_decay=self.optimizer_weight_decay,
         )
+
+    def make_optimizer(self):
+        if self.network == "regression":
+            return self.__build_regression_optimizers()
+        else:
+            return self.__build_quantile_optimizers()
