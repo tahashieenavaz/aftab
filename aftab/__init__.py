73502cbb7d119c7992c39940ebb10002ed954467
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-23 12:39:45 +0200

    adds environments

--- aftab/__init__.py
+++ aftab/__init__.py
@@ -1,2 +1,6 @@
 from .Aftab import Aftab
 from .constants import aftab_seeds
+from .constants import aftab_seeds as SEEDS
+from .constants import aftab_environments
+from .constants import aftab_environments as ENVIRONMENTS
+from .constants import aftab_environments as ENVS
