7bad6335bacd05be39db8cd0ec9a0e38842666c5
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-03 13:27:01 +0200

    adds channel support to Stream activation

--- aftab/modules/Stream.py
+++ aftab/modules/Stream.py
@@ -19,16 +19,28 @@ class Stream(torch.nn.Module):
         self.first_linear = torch.nn.Linear(input_dimension, hidden_dimension)
         self.second_linear = torch.nn.Linear(hidden_dimension, output_dimension)
 
-        self.activation = self.__initiate_activation(activation_class=activation)
+        self.activation = self.__initiate_activation(
+            activation_class=activation, channels=hidden_dimension
+        )
 
         if normalization:
             self.normalization_layer = torch.nn.LayerNorm(hidden_dimension)
 
-    def __initiate_activation(self, activation_class: ModuleType) -> torch.nn.Module:
+    def __initiate_activation(
+        self, activation_class: ModuleType, channels: int
+    ) -> torch.nn.Module:
+        activation_args = {}
         args = inspect.signature(activation_class).parameters
+
         if "inplace" in args:
-            return activation_class(inplace=True)
-        return activation_class()
+            activation_args.update({"inplace": True})
+
+        if "channels" in args:
+            activation_args.update({"channels": channels})
+        elif "in_channels" in args:
+            activation_args.update({"in_channels": channels})
+
+        return activation_class(**activation_args)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.first_linear(x)
