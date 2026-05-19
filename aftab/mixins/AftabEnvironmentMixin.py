c7012f571bc99abf9e149e36d0311fee82fd61d6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-19 16:44:43 +0200

    fixes cpu allocation

--- aftab/mixins/AftabEnvironmentMixin.py
+++ aftab/mixins/AftabEnvironmentMixin.py
@@ -6,73 +6,58 @@ from .AftabBaseMixin import AftabBaseMixin
 class _SplitEnvPool:
     def __init__(self, environment, train_environments: int, test_environments: int):
         self.environment = environment
-        self.train_environments = train_environments
-        self.test_environments = test_environments
-        self.total_environments = train_environments + test_environments
+        self.train_envs = train_environments
+        self.total_envs = train_environments + test_environments
         self.action_space = environment.action_space
         self.observation_space = environment.observation_space
-        self._actions = numpy.empty(self.total_environments, dtype=numpy.int64)
+        self._actions = numpy.empty(self.total_envs, dtype=numpy.int64)
+        self.train_slice = slice(0, self.train_envs)
+        self.test_slice = slice(self.train_envs, self.total_envs)
 
-    def __split_info(self, info, start: int, end: int):
-        if info is None:
+    def _split_info(self, info, slc: slice):
+        if not info:
             return {}
 
-        split_info = {}
-        for key, value in info.items():
-            if (
-                hasattr(value, "shape")
-                and len(value.shape) > 0
-                and value.shape[0] == self.total_environments
-            ):
-                split_info[key] = value[start:end]
-            elif (
-                isinstance(value, (list, tuple))
-                and len(value) == self.total_environments
-            ):
-                split_info[key] = value[start:end]
-            else:
-                split_info[key] = value
-        return split_info
+        return {
+            k: (
+                v[slc]
+                if (getattr(v, "shape", ()) and v.shape[0] == self.total_envs)
+                or (isinstance(v, (list, tuple)) and len(v) == self.total_envs)
+                else v
+            )
+            for k, v in info.items()
+        }
 
     def reset_split(self):
         observation, info = self.environment.reset()
-        train_slice = slice(0, self.train_environments)
-        test_slice = slice(self.train_environments, self.total_environments)
         return (
-            (
-                observation[train_slice],
-                self.__split_info(info, train_slice.start, train_slice.stop),
-            ),
-            (
-                observation[test_slice],
-                self.__split_info(info, test_slice.start, test_slice.stop),
-            ),
+            (observation[self.train_slice], self._split_info(info, self.train_slice)),
+            (observation[self.test_slice], self._split_info(info, self.test_slice)),
         )
 
     def step_split(self, train_actions, test_actions):
-        self._actions[: self.train_environments] = train_actions
-        if self.test_environments > 0:
-            self._actions[self.train_environments :] = test_actions
+        self._actions[self.train_slice] = train_actions
+        if self.total_envs > self.train_envs:  # Only assign if test environments exist
+            self._actions[self.test_slice] = test_actions
 
         observation, reward, termination, truncation, info = self.environment.step(
             self._actions
         )
-        train_slice = slice(0, self.train_environments)
-        test_slice = slice(self.train_environments, self.total_environments)
+
         return (
             (
-                observation[train_slice],
-                reward[train_slice],
-                termination[train_slice],
-                truncation[train_slice],
-                self.__split_info(info, train_slice.start, train_slice.stop),
+                observation[self.train_slice],
+                reward[self.train_slice],
+                termination[self.train_slice],
+                truncation[self.train_slice],
+                self._split_info(info, self.train_slice),
             ),
             (
-                observation[test_slice],
-                reward[test_slice],
-                termination[test_slice],
-                truncation[test_slice],
-                self.__split_info(info, test_slice.start, test_slice.stop),
+                observation[self.test_slice],
+                reward[self.test_slice],
+                termination[self.test_slice],
+                truncation[self.test_slice],
+                self._split_info(info, self.test_slice),
             ),
         )
 
@@ -84,13 +69,20 @@ class AftabEnvironmentMixin(AftabBaseMixin):
     def __init__(self):
         super().__init__()
 
+    def _get_optimal_threads(self, allocation: int) -> int:
+        return max(1, min(16, allocation))
+
     def __make_shared_environment(self, environment: str, seed: int):
+        num_threads = self._get_optimal_threads(self.cpu_count)
+
         combined_environment = envpool.make(
             environment,
-            env_type="gymnasium",
+            env_type=(
+                "grid" if "gymnasium" not in envpool.list_all_envs() else "gymnasium"
+            ),
             num_envs=self.total_environments,
             seed=seed,
-            num_threads=max(1, self.cpu_count),
+            num_threads=num_threads,
             thread_affinity_offset=0,
             noop_max=self.noop,
             reward_clip=self.train_reward_clip,
@@ -103,23 +95,29 @@ class AftabEnvironmentMixin(AftabBaseMixin):
             train_environments=self.train_environments,
             test_environments=self.test_environments,
         )
-        action_dimension = split_environment.action_space.n
-        observation_shape = split_environment.observation_space.shape
-        return split_environment, None, action_dimension, observation_shape
+        return (
+            split_environment,
+            None,
+            split_environment.action_space.n,
+            split_environment.observation_space.shape,
+        )
 
     def make_environments(self, environment: str, seed: int):
-        if bool(getattr(self, "shared_envpool", True)):
+        if getattr(self, "shared_envpool"):
             return self.__make_shared_environment(environment=environment, seed=seed)
 
-        test_cpu_count = min(self.min_cpu_count, max(1, self.cpu_count - 1))
-        train_cpu_count = max(1, self.cpu_count - test_cpu_count)
+        raw_test_cpu = min(self.min_cpu_count, max(1, self.cpu_count - 1))
+        raw_train_cpu = max(1, self.cpu_count - raw_test_cpu)
+
+        train_threads = self._get_optimal_threads(raw_train_cpu)
+        test_threads = self._get_optimal_threads(raw_test_cpu)
 
         train_environment = envpool.make(
             environment,
             env_type="gymnasium",
             num_envs=self.train_environments,
             seed=seed,
-            num_threads=train_cpu_count,
+            num_threads=train_threads,
             thread_affinity_offset=0,
             noop_max=self.noop,
             reward_clip=self.train_reward_clip,
@@ -133,14 +131,17 @@ class AftabEnvironmentMixin(AftabBaseMixin):
             env_type="gymnasium",
             num_envs=self.test_environments,
             seed=seed + 1000,
-            num_threads=test_cpu_count,
-            thread_affinity_offset=train_cpu_count if self.cpu_count > 1 else 0,
+            num_threads=test_threads,
+            thread_affinity_offset=train_threads if self.cpu_count > 1 else 0,
             noop_max=self.noop,
             reward_clip=self.test_reward_clip,
             episodic_life=self.test_episodic_life,
             frame_skip=self.frame_skip,
             stack_num=self.frame_stack,
         )
-        action_dimension = train_environment.action_space.n
-        observation_shape = train_environment.observation_space.shape
-        return train_environment, test_environment, action_dimension, observation_shape
+        return (
+            train_environment,
+            test_environment,
+            train_environment.action_space.n,
+            train_environment.observation_space.shape,
+        )
