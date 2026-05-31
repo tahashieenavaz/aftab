245bb5c6e4ca2a63ca44e7e45d9a246d0408bdda
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-31 13:47:23 +0200

    fixes version in init

--- aftab/__init__.py
+++ aftab/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 from .Aftab import Aftab
 
 from .constants import seeds
@@ -8,4 +10,4 @@ from .constants import environments
 from .constants import environments as ENVS
 from .constants import environments as aftab_environments
 
-__version__ = "0.1.59"
+__version__ = version("aftab")
