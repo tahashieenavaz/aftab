a0d67d5ce2e9e7c4c30d5edfef5a508ff230f7b9
tahashieenavaz <tahashieenavaz@gmail.com>
2026-06-13 20:17:09 +0200

    adds in channels to alpha encoder

--- aftab/encoders/AlphaEncoder.py
+++ aftab/encoders/AlphaEncoder.py
@@ -4,11 +4,16 @@ from aftab.constants import ModuleType
 
 
 class AlphaEncoder(torch.nn.Module):
-    def __init__(self, *, activation: ModuleType = torch.nn.ReLU):
+    def __init__(self, *, activation: ModuleType = torch.nn.ReLU, in_channels: int = 4):
         super().__init__()
         self.stream = torch.nn.Sequential(
             EncoderBlock(
-                4, 32, kernel_size=4, stride=2, padding=1, activation=activation
+                in_channels,
+                32,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                activation=activation,
             ),
             EncoderBlock(
                 32, 64, kernel_size=4, stride=2, padding=1, activation=activation
