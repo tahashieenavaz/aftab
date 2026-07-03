c3219cbba70bc052dfa16c98d1b08d6be6e768e6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-03 22:36:20 +0200

    fixes hadamax block

--- aftab/modules/HadamaxBlock.py
+++ aftab/modules/HadamaxBlock.py
@@ -1,6 +1,7 @@
 import torch
+import torch.nn.functional as F
 from aftab.constants import ModuleType
-from .LayerNorm2d import LayerNorm2d
+from .HadamaxLayerNorm2d import HadamaxLayerNorm2d
 from .MixedActivation import MixedActivation
 
 
@@ -22,45 +23,43 @@ class HadamaxBlock(torch.nn.Module):
         super().__init__()
         self.mixed = mixed
 
-        if self.mixed:
-            self.convolutional_a = torch.nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-                bias=False,
-            )
-            self.convolutional_b = torch.nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size=1,
-                stride=stride,
-                padding=0,
-                bias=False,
-            )
-        else:
-            self.convolutional = torch.nn.Conv2d(
-                in_channels,
-                out_channels * 2,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-                bias=False,
-            )
+        self.convolutional = torch.nn.Conv2d(
+            in_channels,
+            out_channels * 2,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=False,
+        )
 
-        self.normalization = LayerNorm2d(in_channels)
+        self.normalization = HadamaxLayerNorm2d(out_channels)
         self.pool = torch.nn.MaxPool2d(
             kernel_size=pool_kernel, stride=pool_stride, padding=pool_padding
         )
+
+        if self.mixed:
+            self.avg_pool = torch.nn.AvgPool2d(
+                kernel_size=pool_kernel, stride=pool_stride, padding=pool_padding
+            )
+            self.pool_mix = torch.nn.Parameter(torch.tensor(0.0))
+            self.cross_talk = torch.nn.Parameter(torch.zeros(1, out_channels, 1, 1))
+
         self.chi = chi(out_channels, dim=1) if chi is MixedActivation else chi()
         self.psi = psi(out_channels, dim=1) if psi is MixedActivation else psi()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.normalization(x)
+        x = self.normalization(self.convolutional(x))
         if self.mixed:
-            adam = self.convolutional_a(x)
-            eve = self.convolutional_b(x)
+            B, C, H, W = x.shape
+            x = x.view(B, 2, C // 2, H, W).transpose(1, 2).contiguous().view(B, C, H, W)
+        adam, eve = x.chunk(2, dim=1)
+        if self.mixed:
+            adam = adam + self.cross_talk * eve
+        gated = self.chi(adam).mul_(self.psi(eve))
+        if self.mixed:
+            mix_weight = torch.sigmoid(self.pool_mix)
+            return mix_weight * self.pool(gated) + (1.0 - mix_weight) * self.avg_pool(
+                gated
+            )
         else:
-            adam, eve = self.convolutional(x).chunk(2, 1)
-        return self.pool(self.chi(adam).mul_(self.psi(eve)))
+            return self.pool(gated)
