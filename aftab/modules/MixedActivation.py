961a836949e857c4441f213d9cfeb5a9801c56f9
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-03 21:46:39 +0200

    enhances mixed activation

--- aftab/modules/MixedActivation.py
+++ aftab/modules/MixedActivation.py
@@ -3,21 +3,21 @@ import torch.nn.functional as F
 
 
 class MixedActivation(torch.nn.Module):
-    def __init__(self, hidden_dimension: int):
+    def __init__(self, hidden_dimension: int, dim: int = -1):
         super().__init__()
+        self.hidden_dimension = hidden_dimension
+        self.dim = dim
         self.logits = torch.nn.Parameter(torch.zeros(hidden_dimension, 3))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         weights = torch.softmax(self.logits, dim=-1)
-        w_gelu, w_silu, w_relu = weights[:, 0], weights[:, 1], weights[:, 2]
+        w_gelu, w_silu, w_relu = torch.unbind(weights, dim=-1)
 
-        if x.dim() > 1:
-            broadcast_shape = [1, x.size(1)] + [1] * (x.dim() - 2)
-        else:
-            broadcast_shape = [x.size(0)]
-
-        w_gelu = w_gelu.view(*broadcast_shape)
-        w_silu = w_silu.view(*broadcast_shape)
-        w_relu = w_relu.view(*broadcast_shape)
+        if self.dim != -1 and self.dim != (x.dim() - 1):
+            shape = [1] * x.dim()
+            shape[self.dim] = self.hidden_dimension
+            w_gelu = w_gelu.view(*shape)
+            w_silu = w_silu.view(*shape)
+            w_relu = w_relu.view(*shape)
 
         return w_gelu * F.gelu(x) + w_silu * F.silu(x) + w_relu * F.relu(x)
