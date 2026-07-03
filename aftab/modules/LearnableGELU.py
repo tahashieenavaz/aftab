e227d68573a00a488668bf0353f28f9f9c9398c5
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-04 00:11:08 +0200

    fixes learnable gelu to use fused F.gelu

--- aftab/modules/LearnableGELU.py
+++ aftab/modules/LearnableGELU.py
@@ -1,18 +1,19 @@
 import torch
-import math
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class LearnableGELU(torch.nn.Module):
-    def __init__(self, in_channels: int, dim: int = -1):
+class LearnableGELU(nn.Module):
+    def __init__(self, in_channels: int, dim: int = -1, min_lambda: float = 1e-4):
         super().__init__()
         self.in_channels = in_channels
         self.dim = dim
-        self.lambdas = torch.nn.Parameter(torch.ones(in_channels))
+        self.min_lambda = min_lambda
+        self.lambdas = nn.Parameter(torch.ones(in_channels))
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         shape = [1] * x.ndim
         shape[self.dim] = self.in_channels
         lam = self.lambdas.view(*shape)
-        z = lam * x
-        cdf = 0.5 * (1.0 + torch.erf(z / math.sqrt(2.0)))
-        return x * cdf
+        lam = torch.clamp(lam, min=self.min_lambda)
+        return F.gelu(x * lam) / lam
