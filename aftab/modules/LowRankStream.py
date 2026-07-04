3a0f588d8ed72afc716760352f458d516b17dacb
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-04 15:01:17 +0200

    rewrites low rank stream

--- aftab/modules/LowRankStream.py
+++ aftab/modules/LowRankStream.py
@@ -1,5 +1,5 @@
 import torch
-from aftab.constants import ModuleType
+from typing import Type
 
 
 class LowRankStream(torch.nn.Module):
@@ -10,25 +10,31 @@ class LowRankStream(torch.nn.Module):
         hidden_dimension: int,
         output_dimension: int,
         rank: int,
-        activation: ModuleType,
+        activation: Type[torch.nn.Module],
         normalization: bool,
+        dropout: float = 0.0,
     ):
         super().__init__()
-        self.normalization = normalization
         self.first_linear_down = torch.nn.Linear(input_dimension, rank, bias=False)
-        self.first_linear_up = torch.nn.Linear(rank, hidden_dimension)
-        self.second_linear_down = torch.nn.Linear(hidden_dimension, rank, bias=False)
-        self.second_linear_up = torch.nn.Linear(rank, output_dimension)
+        self.first_linear_up = torch.nn.Linear(
+            rank, hidden_dimension, bias=not normalization
+        )
+        self.normalization_layer = (
+            torch.nn.LayerNorm(hidden_dimension)
+            if normalization
+            else torch.nn.Identity()
+        )
         self.activation = activation()
-        if normalization:
-            self.normalization_layer = torch.nn.LayerNorm(hidden_dimension)
+        self.dropout = torch.nn.Dropout(dropout) if dropout > 0 else torch.nn.Identity()
+        self.second_linear_down = torch.nn.Linear(hidden_dimension, rank, bias=False)
+        self.second_linear_up = torch.nn.Linear(rank, output_dimension, bias=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.first_linear_down(x)
         x = self.first_linear_up(x)
-        if self.normalization:
-            x = self.normalization_layer(x)
+        x = self.normalization_layer(x)
         x = self.activation(x)
+        x = self.dropout(x)
         x = self.second_linear_down(x)
         x = self.second_linear_up(x)
         return x
