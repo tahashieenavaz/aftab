ba7fcf0217b96aa534c34fa1fdfdfae124ab808e
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-20 14:35:49 +0200

    adds environment aliases

--- aftab/__init__.py
+++ aftab/__init__.py
@@ -4,7 +4,11 @@ from .constants import seeds
 from .constants import seeds as SEEDS
 from .constants import seeds as aftab_seeds
 
+from .constants import atari_environments
 from .constants import environments
+from .constants import procgen_environments
+from .constants import atari_environments as ATARI_ENVS
+from .constants import procgen_environments as PROCGEN_ENVS
 from .constants import environments as ENVS
 from .constants import environments as aftab_environments
 
